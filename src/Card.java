4367ee41bba541bbf5b712841661f026a7ddd268
Taeseung Park <Taeseung Park@172.31.113.169>
2019-12-09 15:11:31 -0500

    Edit 12-9-19

---
+++ src/Card.java
@@ -0,0 +1,30 @@
+public class Card {
+	
+	public final static int Clubs = 0;
+	public final static int Diamonds = 1;
+	public final static int Hearts = 2;
+	public final static int Spades = 3;
+	
+	public final static int Ace = 1;
+	public final static int Jack = 11;
+	public final static int Queen = 12;
+	public final static int King = 13;
+	
+	private final int suit;
+	
+	private final int value;
+	
+	public Card() {
+		suit = 0;
+		value = 1;
+	}
+	
+	public Card(int theSuit, int theValue) {
+		
+		
+	}
+	
+	
+	
+	
+}
