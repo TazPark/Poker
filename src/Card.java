d2fdefe589e4ac62a83a12cd5933bbda9741f4e7
Taeseung Park <Taeseung Park@10.0.0.32>
2019-12-14 18:03:10 -0500

    Finished 12/14/19 6:03

--- src/Card.java
+++ src/Card.java
@@ -33,20 +33,32 @@ public class Card {
 	}
 	
 	public String suitToString() {
-		switch(suit)
+		switch(suit) {
 			case 0: return "Clubs";
 			case 1: return "Diamonds";
 			case 2: return "Hearts";
 			case 3: return "Spades";
+			default: return "tempSuit";
+		}
 	}
 	
 	public String valueToString() {
-		switch(value)
+		switch(value) {
 			case 1: return "Ace";
+			case 2: return "2";
+			case 3: return "3";
+			case 4: return "4";
+			case 5: return "5";
+			case 6: return "6";
+			case 7: return "7";
+			case 8: return "8";
+			case 9: return "9";
+			case 10: return "10";
 			case 11: return "Jack";
 			case 12: return "Queen";
 			case 13: return "King";
-			default: return value;
+			default: return "Joker";
+		}
 	}
 	
 	public String toString() {
