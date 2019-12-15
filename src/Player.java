319a9d22c4330b2a645895db4e5f962ed6c1ec2c
Taeseung Park <Taeseung Park@10.0.0.32>
2019-12-14 22:11:00 -0500

    blanace

--- src/Player.java
+++ src/Player.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Player {
 	
 	private Hand playerHand;
-	private float blanace;
+	private float balance;
 	private String name;
 	
 	public Player(String inputName, float balance) {
 		this.playerHand = new Hand();
 		this.name = inputName;
-		this.blanace = balance;
+		this.balance = balance;
 	}
 	
 	public String getName() {
@@ -30,17 +30,17 @@ public class Player {
 		if(!this.name.equalsIgnoreCase("Bot Negreanu")) {
 			wage = keyboard.nextFloat();
 			if(wage >= min) {
-				blanace -= wage;
+				balance -= wage;
 				return wage;
-			} else if (wage < min && this.blanace >= min){
+			} else if (wage < min && this.balance >= min){
 				do {
 					System.out.println("Wage cannot be lower than the min.");
 					System.out.println("Please enter a valid wage: ");
 					wage = keyboard.nextFloat();
 				}while(wage < min);
-				blanace -= wage;
+				balance -= wage;
 				return wage;
-			} else if (wage < min && this.blanace < min) {
+			} else if (wage < min && this.balance < min) {
 				System.out.println("Since you cannot wage the minimum, you will have to go all in");
 				System.out.println("To go all in, type your full balance (Your balance: " + this.getBalance() +")");
 				wage = keyboard.nextFloat();
@@ -49,7 +49,7 @@ public class Player {
 						System.out.println("Invalid wage.");
 						System.out.println("Input the correct wage: " + this.getBalance());
 						wage = keyboard.nextFloat();
-					}while(wage != blanace);
+					}while(wage != balance);
 					return wage;
 				}
 			}
@@ -57,84 +57,84 @@ public class Player {
 		
 		//for computer
 		if(playerHand.hasStraight() == true && playerHand.hasFlush() == true) {
-			wage = this.blanace;
-			this.blanace = this.blanace - this.blanace;
+			wage = this.balance;
+			this.balance = this.balance - this.balance;
 			return wage;
 		}
 		if(playerHand.hasFourOfAKind() == true) {
-			if((this.blanace*.7) >= min) {
-				wage = this.blanace * .7f;
-				this.blanace -= wage;
+			if((this.balance*.7) >= min) {
+				wage = this.balance * .7f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.hasFullHouse() == true) {
-			if((this.blanace*.6) >= min) {
-				this.blanace = this.blanace * .6f;
-				this.blanace -= wage;
+			if((this.balance*.6) >= min) {
+				this.balance = this.balance * .6f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.hasFlush() == true) {
-			if((this.blanace*.5) >= min) {
-				wage = this.blanace * .5f;
-				this.blanace -= wage;
+			if((this.balance*.5) >= min) {
+				wage = this.balance * .5f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.hasStraight() == true) {
-			if((this.blanace) >= min) {
-				wage = this.blanace * .4f;
-				this.blanace -= wage;
+			if((this.balance) >= min) {
+				wage = this.balance * .4f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.hasTriplet() == true) {
-			if((this.blanace*.3) >= min) {
-				wage = this.blanace * .3f;
-				this.blanace -= wage;
+			if((this.balance*.3) >= min) {
+				wage = this.balance * .3f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.numPairs() == 2) {
-			if((this.blanace*.2) >= min) {
-				wage = this.blanace * .2f;
-				this.blanace -= wage;
+			if((this.balance*.2) >= min) {
+				wage = this.balance * .2f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
 		if(playerHand.numPairs() == 1) {
-			if((this.blanace*.1) >= min) {
-				wage = this.blanace * .1f;
-				this.blanace -= wage;
+			if((this.balance*.1) >= min) {
+				wage = this.balance * .1f;
+				this.balance -= wage;
 				return wage;
 			}
 			else {
-				this.blanace -= min;
+				this.balance -= min;
 				return min;
 			}
 		}
@@ -146,10 +146,10 @@ public class Player {
 	}
 	
 	public float getBalance() {
-		return this.blanace;
+		return this.balance;
 	}
 	
 	public void winnings(float amount){
-		this.blanace += amount;
+		this.balance += amount;
 	}
 }
